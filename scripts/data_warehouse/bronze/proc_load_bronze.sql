034f0cf3e345bff17fd951ac4aa2a59dcbaa7eea
Nitin Kunigal <nitinkunigal@gmail.com>
2025-05-29 17:40:42 +0530

    Update proc_load_bronze.sql

--- scripts/data_warehouse/bronze/proc_load_bronze.sql
+++ scripts/data_warehouse/bronze/proc_load_bronze.sql
@@ -44,7 +44,7 @@ BEGIN
 		TRUNCATE TABLE bronze.crm_cust_info;
 		PRINT '>> Inserting Data Into: bronze.crm_cust_info';
 		BULK INSERT bronze.crm_cust_info
-		FROM 'D:\Data Analytics - SUPER IMP\SQL\Baraa\The Complete SQL Bootcamp - Udemy\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		FROM 'C:\sql\dwh_project\datasets\source_crm\cust_info.csv'
 		WITH (      -- 'WITH' clause tell SQL how to handle our CSV File
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -59,7 +59,7 @@ BEGIN
 		TRUNCATE TABLE bronze.crm_prd_info;
 		PRINT '>> Inserting Data Into: bronze.crm_prd_info';
 		BULK INSERT bronze.crm_prd_info
-		FROM 'D:\Data Analytics - SUPER IMP\SQL\Baraa\The Complete SQL Bootcamp - Udemy\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		FROM 'C:\sql\dwh_project\datasets\source_crm\prd_info.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -74,7 +74,7 @@ BEGIN
 		TRUNCATE TABLE bronze.crm_sales_details;
 		PRINT '>> Inserting Data Into: bronze.crm_sales_details';
 		BULK INSERT bronze.crm_sales_details
-		FROM 'D:\Data Analytics - SUPER IMP\SQL\Baraa\The Complete SQL Bootcamp - Udemy\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		FROM 'C:\sql\dwh_project\datasets\source_crm\sales_details.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -93,7 +93,7 @@ BEGIN
 		TRUNCATE TABLE bronze.erp_cust_az12;
 		PRINT '>> Inserting Data Into: bronze.erp_cust_az12';
 		BULK INSERT bronze.erp_cust_az12
-		FROM 'D:\Data Analytics - SUPER IMP\SQL\Baraa\The Complete SQL Bootcamp - Udemy\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+		FROM 'C:\sql\dwh_project\datasets\source_erp\cust_az12.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -108,7 +108,7 @@ BEGIN
 		TRUNCATE TABLE bronze.erp_loc_a101;
 		PRINT '>> Inserting Data Into: bronze.erp_loc_a101';
 		BULK INSERT bronze.erp_loc_a101
-		FROM 'D:\Data Analytics - SUPER IMP\SQL\Baraa\The Complete SQL Bootcamp - Udemy\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+		FROM 'C:\sql\dwh_project\datasets\source_erp\loc_a101.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -123,7 +123,7 @@ BEGIN
 		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 		PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
 		BULK INSERT bronze.erp_px_cat_g1v2
-		FROM 'D:\Data Analytics - SUPER IMP\SQL\Baraa\The Complete SQL Bootcamp - Udemy\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+		FROM 'C:\sql\dwh_project\datasets\source_erp\px_cat_g1v2.csv'
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
